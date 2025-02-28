2dcbeba948558840ff6a6b5977b0fb96688a60ca
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2025-02-28 12:09:41 +0200

    x-cloak

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -93,13 +93,13 @@
                         aria-controls="mobile-menu" aria-expanded="false">
                         <span class="sr-only">Deschide meniul principal</span>
                         <!-- Icon when menu is closed -->
-                        <svg x-show="!mobileMenuOpen" class="block w-6 h-6" xmlns="http://www.w3.org/2000/svg"
+                        <svg x-cloak x-show="!mobileMenuOpen" class="block w-6 h-6" xmlns="http://www.w3.org/2000/svg"
                             fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
                         <!-- Icon when menu is open -->
-                        <svg x-show="mobileMenuOpen" class="block w-6 h-6" xmlns="http://www.w3.org/2000/svg"
+                        <svg x-cloak x-show="mobileMenuOpen" class="block w-6 h-6" xmlns="http://www.w3.org/2000/svg"
                             fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M6 18L18 6M6 6l12 12" />
